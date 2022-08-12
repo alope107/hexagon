d551613610c62cf3ac37b0851a3a110e407ab07e
Auberon <auberon.lopez@gmail.com>
2022-08-11 19:49:47 -0700

    Added some more experimentation with sound

--- sounds.js
+++ sounds.js
@@ -1,25 +1,62 @@
 const audioCtx = new AudioContext();
 
+console.log(math.complex(1, 2));
+
 const choose = (arr) => {
     return arr[Math.floor(Math.random()*arr.length)];
 }
 
+const buffabeef = () => {
+    // Yoinked from https://developer.mozilla.org/en-US/docs/Web/API/BaseAudioContext/createBuffer 
+    // Create an empty three-second stereo buffer at the sample rate of the AudioContext
+    const myArrayBuffer = audioCtx.createBuffer(2, audioCtx.sampleRate * 3, audioCtx.sampleRate);
+    console.log("sampleRate", audioCtx.sampleRate);
+
+    // Fill the buffer with sine wave
+    for (let channel = 0; channel < myArrayBuffer.numberOfChannels; channel++) {
+    // This gives us the actual ArrayBuffer that contains the data
+    const nowBuffering = myArrayBuffer.getChannelData(channel);
+    const desiredFreq = 440; // in hz
+    for (let i = 0; i < myArrayBuffer.length; i++) {
+        nowBuffering[i] = Math.sin(i*((2*Math.PI*desiredFreq) / audioCtx.sampleRate));
+    }
+    }
+
+    // Get an AudioBufferSourceNode.
+    // This is the AudioNode to use when we want to play an AudioBuffer
+    const source = audioCtx.createBufferSource();
+    // set the buffer in the AudioBufferSourceNode
+    source.buffer = myArrayBuffer;
+    // connect the AudioBufferSourceNode to the
+    // destination so we can hear the sound
+    source.connect(audioCtx.destination);
+    // start the source playing
+    source.start();
+}
+
 const boop = async () => {
     const osc = audioCtx.createOscillator();
-    osc.type = 'square';
+    osc.type = 'sine';
     //osc.connect(audioCtx.destination);
     const freqs = [262, 294.8, 327.5, 349.3, 393, 436.7, 491.2, 524, 589.5];
-    osc.frequency.value = choose(freqs);
+    //osc.frequency.value = choose(freqs);
+    osc.frequency.value = 440;
 
-    const g = audioCtx.createGain();
-    osc.connect(g);
-    g.connect(audioCtx.destination);
+    osc.connect(audioCtx.destination);
     osc.start();
 
-    const ramp = 3;
-    const finishTime = audioCtx.currentTime + ramp;
-    g.gain.exponentialRampToValueAtTime(.00001, finishTime);
-    osc.stop(finishTime);
+    await new Promise(r => setTimeout(r,1000))
+    osc.stop();
+
+    // const g = audioCtx.createGain();
+    // osc.connect(g);
+    // g.connect(audioCtx.destination);
+    // osc.start();
+
+    // const ramp = 3;
+    // const finishTime = audioCtx.currentTime + ramp;
+    // g.gain.exponentialRampToValueAtTime(.00001, finishTime);
+    // osc.stop(finishTime);
 
     
     // await new Promise(r => setTimeout(r, 300));
